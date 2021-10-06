199f34ed97bcbd63c3f1ec25d67d2fb804e97fbe
HohenHeizen <72352228+HohenHeizen@users.noreply.github.com>
2021-10-06 15:41:05 -0500

    v1.2

--- 1.3/Source/HHStoryteller.cs
+++ 1.3/Source/HHStoryteller.cs
@@ -1,24 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Reflection;
+﻿
 using System.Linq;
 using HugsLib;
 using RimWorld;
-using RimWorld.Planet;
+
 using Verse;
-using Verse.AI;
-using Verse.Sound;
+
 using HarmonyLib;
-using System.Text;
-using UnityEngine;
-using HugsLib.Utils;
-using Verse.AI.Group;
-using HugsLib.Settings;
-using RimWorld.QuestGen;
-using System.Net;
-using System.IO;
-using System.Collections;
-using System.Reflection.Emit;
+
 
 
 namespace HHStoryteller
@@ -35,48 +23,6 @@ namespace HHStoryteller
             get { return "HHStoryteller"; }
         }
     }
-
-
-    [HarmonyPatch(typeof(PawnGroupMakerUtility), "TryGetRandomFactionForCombatPawnGroup")]
-    public static class NoDeadFactions
-    {
-        [HarmonyPrefix]
-        public static bool DisableMethod()
-        {
-            return false;
-        }
-
-        [HarmonyPostfix]
-        public static void Replace(ref bool __result, float points, out Faction faction,
-            Predicate<Faction> validator = null, bool allowNonHostileToPlayer = false, bool allowHidden = false,
-            bool allowDefeated = false, bool allowNonHumanlike = true)
-        {
-            List<Faction> source = WorldSwitchUtility.FactionsOnCurrentWorld(Find.FactionManager.AllFactions).Where(
-                delegate (Faction f) {
-                    int arg_E3_0;
-                    if ((allowHidden || !f.def.hidden) && (allowDefeated || !f.defeated) &&
-                        (allowNonHumanlike || f.def.humanlikeFaction) &&
-                        (allowNonHostileToPlayer || f.HostileTo(Faction.OfPlayer)) &&
-                        f.def.pawnGroupMakers != null)
-                    {
-                        if (f.def.pawnGroupMakers.Any((PawnGroupMaker x) =>
-                            x.kindDef == PawnGroupKindDefOf.Combat) && (validator == null || validator(f)))
-                        {
-                            arg_E3_0 = ((points >= f.def.MinPointsToGeneratePawnGroup(PawnGroupKindDefOf.Combat))
-                                ? 1
-                                : 0);
-                            return arg_E3_0 != 0;
-                        }
-                    }
-
-                    arg_E3_0 = 0;
-                    return arg_E3_0 != 0;
-                }).ToList<Faction>();
-            __result = source.TryRandomElementByWeight((Faction f) => f.def.RaidCommonalityFromPoints(points),
-                out faction);
-        }
-    }
-
     [HarmonyPatch(typeof(Map), "get_PlayerWealthForStoryteller")]
     public static class TechIsWealth
     {
